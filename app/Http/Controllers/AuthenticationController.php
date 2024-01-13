f4661c2709196f533c4759adc8df1444440718fa
Ibrahim Khalil Shakil <iksakil2@gmail.com>
2024-01-13 22:23:38 +0600

    added authenticationcontroller and issuperadmin middleware

---
+++ app/Http/Controllers/AuthenticationController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Role;
+use App\Models\User;
+use App\Http\Requests\Authentication\SignUpRequest;
+use App\Http\Requests\Authentication\SignInRequest;
+use Illuminate\Support\Facades\Hash;
+use Exception;
+
+class AuthenticationController extends Controller
+{
+    public function signUpForm()
+    {
+        return view('authentication.register');
+    }
+
+    public function signUpStore(SignUpRequest $request)
+    {
+        try {
+            $user = new User;
+            $user->name = $request->fullName;
+            $user->email = $request->emailAddress;
+            $user->contact_number = $request->contactNumber;
+            $user->password = Hash::make($request->password);
+            $user->role_id = 5;
+            if ($user->save())
+                return redirect('login')->with('success', 'Registration Completed');
+            else
+                return redirect('login')->with('error', 'Data is not Saved. Please try again');
+        } catch (Exception $e) {
+            return redirect('login')->with('error', 'Registration Failed. Please try again');
+            dd($e);
+        }
+    }
+
+    public function signInForm()
+    {
+        return view('authentication.login');
+    }
+
+    public function signInCheck(SignInRequest $request)
+    {
+        try {
+            $user = User::where('email', $request->username)->orWhere('contact_number', $request->username)->first();
+            if ($user) {
+                if (Hash::check($request->password, $user->password)) {
+                    $this->setSession($user);
+                    return redirect()->route('dashboard')->with('success', 'Successfully Loged In');
+                } else
+                    return redirect()->route('login')->with('error', 'Incorrect Password! Login Failed.');
+            } else
+                return redirect()->route('login')->with('error', 'Invalid Username. Please try again');
+        } catch (Exception $e) {
+            return redirect('login')->with('error', 'Login Failed. Please try again');
+            dd($e);
+        }
+    }
+
+    public function setSession($user){
+        return request()->session()->put([
+            'userId'=>encryptor('encrypt',$user->id),
+            'userName'=>encryptor('encrypt',$user->name),
+            'role'=>encryptor('encrypt',$user->role->type),
+            'roleIdentity'=>encryptor('encrypt',$user->role->identity),
+            'language' => encryptor('encrypt', $user->language),
+            'image'=> $user->image??'No Image Found'
+        ]);
+    }
+
+    public function signOut(){
+       request()->session()->flush();
+       return redirect('login')->with($this->resMessageHtml(false, 'error', currentUserId()));
+    }
+}
--- app/Http/Middleware/isSuperAdmin.php
+++ app/Http/Middleware/isSuperAdmin.php
@@ -5,6 +5,8 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use App\Models\User;
+use Session;
 
 class isSuperAdmin
 {
@@ -15,6 +17,19 @@ class isSuperAdmin
      */
     public function handle(Request $request, Closure $next): Response
     {
-        return $next($request);
+        if (!Session::has('userId') || Session::has('userId') == null || !Session::has('roleIdentity')) {
+            return redirect()->route('logOut');
+        } else {
+            $user = User::findOrFail(currentUserId());
+            app()->setLocale($user->language);
+            if (!$user) {
+                return redirect()->route('logOut');
+            } else if (currentUser() != 'superadmin') {
+                return redirect()->back()->with($this->resMessageHtml(false, 'error', 'Access Denied'));
+            } else {
+                return $next($request);
+            }
+        }
+        return redirect()->route('logOut');
     }
 }
