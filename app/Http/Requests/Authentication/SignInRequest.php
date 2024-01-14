69403413eca284cfa3710a6cee2f529fa5194b1d
Ibrahim Khalil Shakil <iksakil2@gmail.com>
2024-01-15 00:30:52 +0600

    added signup and signin requests

--- app/Http/Requests/Authentication/SignInRequest.php
+++ app/Http/Requests/Authentication/SignInRequest.php
@@ -11,7 +11,7 @@ class SignInRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,8 @@ class SignInRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'username'=>'required',
+            'password'=>'required'
         ];
     }
 }
--- app/Http/Requests/Authentication/SignUpRequest.php
+++ app/Http/Requests/Authentication/SignUpRequest.php
@@ -11,7 +11,7 @@ class SignUpRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,10 @@ class SignUpRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'fullName' => 'required|max:255',
+            'emailAddress' => 'required|unique:users,email',
+            'contactNumber' => 'required|unique:users,contact_number',
+            'password' => 'required|confirmed'
         ];
     }
 }
