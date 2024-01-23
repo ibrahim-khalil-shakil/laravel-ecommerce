d7dc294be67008bcb8ead618ce4c49d2783c8df7
Ibrahim Khalil Shakil <iksakil2@gmail.com>
2024-01-23 23:14:51 +0600

    user list done

--- app/Http/Controllers/Backend/UserController.php
+++ app/Http/Controllers/Backend/UserController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
-use App\Models\Category;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -13,7 +13,8 @@ class UserController extends Controller
      */
     public function index()
     {
-        return view('Backend.Users.index');
+        $users=User::get();
+        return view('Backend.Users.index', compact('users'));
     }
 
     /**
--- resources/views/Backend/Users/index.blade.php
+++ resources/views/Backend/Users/index.blade.php
@@ -2,104 +2,91 @@
 @section('title', 'Users')
 
 @section('content')
-    <!-- Main content -->
-    <section class="content">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-12">
-    
-                    <div class="card">
-                        <div class="card-header">
-                            <h3 class="card-title">DataTable with default features</h3>
-                        </div>
-                        <!-- /.card-header -->
-                        <div class="card-body">
-                            <table id="example1" class="table table-bordered table-striped">
-                                <thead>
-                                    <tr>
-                                        <th>Rendering engine</th>
-                                        <th>Browser</th>
-                                        <th>Platform(s)</th>
-                                        <th>Engine version</th>
-                                        <th>CSS grade</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td>Trident</td>
-                                        <td>Internet
-                                            Explorer 4.0
-                                        </td>
-                                        <td>Win 95+</td>
-                                        <td> 4</td>
-                                        <td>X</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Trident</td>
-                                        <td>Internet
-                                            Explorer 5.0
-                                        </td>
-                                        <td>Win 95+</td>
-                                        <td>5</td>
-                                        <td>C</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Trident</td>
-                                        <td>Internet
-                                            Explorer 5.5
-                                        </td>
-                                        <td>Win 95+</td>
-                                        <td>5.5</td>
-                                        <td>A</td>
-                                    </tr>
-                                </tbody>
-                                <tfoot>
-                                    <tr>
-                                        <th>Rendering engine</th>
-                                        <th>Browser</th>
-                                        <th>Platform(s)</th>
-                                        <th>Engine version</th>
-                                        <th>CSS grade</th>
-                                    </tr>
-                                </tfoot>
-                            </table>
-                        </div>
-                        <!-- /.card-body -->
+<!-- Main content -->
+<section class="content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+
+                <div class="card">
+                    <div class="card-header row">
+                     <div class="col-8">
+                        <h3 class="card-title">User List</h3>
+                     </div>
+                     <div class="col-4 text-right">
+                        <a href="" class="btn btn-success">Add New</a>
+                     </div>
                     </div>
-                    <!-- /.card -->
+                    <!-- /.card-header -->
+                    <div class="card-body">
+                        <table id="example1" class="table table-bordered table-striped">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Name</th>
+                                    <th>Role</th>
+                                    <th>Email</th>
+                                    <th>Contact</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @forelse ($users as $user)
+                                <tr>
+                                    <td>{{$user->id}}</td>
+                                    <td>{{$user->name}}</td>
+                                    <td>{{$user?->role->type}}</td>
+                                    <td>{{$user->email}}</td>
+                                    <td> {{$user->contact_number}}</td>
+                                    <td>X</td>
+                                </tr>
+                                @empty
+                            <tfoot>
+                                <tr>
+                                    <th colspan="6">No User Found</th>
+                                </tr>
+                            </tfoot>
+                            @endforelse
+                            </tbody>
+                        </table>
+                    </div>
+                    <!-- /.card-body -->
                 </div>
-                <!-- /.col -->
+                <!-- /.card -->
             </div>
-            <!-- /.row -->
+            <!-- /.col -->
         </div>
-        <!-- /.container-fluid -->
-    </section>
-    <!-- /.content -->
+        <!-- /.row -->
+    </div>
+    <!-- /.container-fluid -->
+</section>
+<!-- /.content -->
 @endsection
 
 @push('styles')
-   <!-- DataTables -->
+<!-- DataTables -->
 <link rel="stylesheet" href="{{asset('public/Backend/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css')}}">
-<link rel="stylesheet" href="{{asset('public/Backend/plugins/datatables-responsive/css/responsive.bootstrap4.min.css')}}">
-<link rel="stylesheet" href="{{asset('public/Backend/plugins/datatables-buttons/css/buttons.bootstrap4.min.css')}}"> 
+<link rel="stylesheet"
+    href="{{asset('public/Backend/plugins/datatables-responsive/css/responsive.bootstrap4.min.css')}}">
+<link rel="stylesheet" href="{{asset('public/Backend/plugins/datatables-buttons/css/buttons.bootstrap4.min.css')}}">
 @endpush
 @push('scripts')
-    <!-- DataTables  & Plugins -->
-    <script src="{{asset('public/Backend/plugins/datatables/jquery.dataTables.min.js')}}"></script>
-    <script src="{{asset('public/Backend/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js')}}"></script>
-    <script src="{{asset('public/Backend/plugins/datatables-responsive/js/dataTables.responsive.min.js')}}"></script>
-    <script src="{{asset('public/Backend/plugins/datatables-responsive/js/responsive.bootstrap4.min.js')}}"></script>
-    <script src="{{asset('public/Backend/plugins/datatables-buttons/js/dataTables.buttons.min.js')}}"></script>
-    <script src="{{asset('public/Backend/plugins/datatables-buttons/js/buttons.bootstrap4.min.js')}}"></script>
-    <script src="{{asset('public/Backend/plugins/jszip/jszip.min.js')}}"></script>
-    <script src="{{asset('public/Backend/plugins/pdfmake/pdfmake.min.js')}}"></script>
-    <script src="{{asset('public/Backend/plugins/pdfmake/vfs_fonts.js')}}"></script>
-    <script src="{{asset('public/Backend/plugins/datatables-buttons/js/buttons.html5.min.js')}}"></script>
-    <script src="{{asset('public/Backend/plugins/datatables-buttons/js/buttons.print.min.js')}}"></script>
-    <script src="{{asset('public/Backend/plugins/datatables-buttons/js/buttons.colVis.min.js')}}"></script>
-    <!-- Page specific script -->
-    <script>
-        $(function () {
+<!-- DataTables  & Plugins -->
+<script src="{{asset('public/Backend/plugins/datatables/jquery.dataTables.min.js')}}"></script>
+<script src="{{asset('public/Backend/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js')}}"></script>
+<script src="{{asset('public/Backend/plugins/datatables-responsive/js/dataTables.responsive.min.js')}}"></script>
+<script src="{{asset('public/Backend/plugins/datatables-responsive/js/responsive.bootstrap4.min.js')}}"></script>
+<script src="{{asset('public/Backend/plugins/datatables-buttons/js/dataTables.buttons.min.js')}}"></script>
+<script src="{{asset('public/Backend/plugins/datatables-buttons/js/buttons.bootstrap4.min.js')}}"></script>
+<script src="{{asset('public/Backend/plugins/jszip/jszip.min.js')}}"></script>
+<script src="{{asset('public/Backend/plugins/pdfmake/pdfmake.min.js')}}"></script>
+<script src="{{asset('public/Backend/plugins/pdfmake/vfs_fonts.js')}}"></script>
+<script src="{{asset('public/Backend/plugins/datatables-buttons/js/buttons.html5.min.js')}}"></script>
+<script src="{{asset('public/Backend/plugins/datatables-buttons/js/buttons.print.min.js')}}"></script>
+<script src="{{asset('public/Backend/plugins/datatables-buttons/js/buttons.colVis.min.js')}}"></script>
+<!-- Page specific script -->
+<script>
+    $(function () {
         $("#example1").DataTable({
           "responsive": true, "lengthChange": false, "autoWidth": false,
           "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
@@ -114,5 +101,5 @@
           "responsive": true,
         });
       });
-    </script>
+</script>
 @endpush
