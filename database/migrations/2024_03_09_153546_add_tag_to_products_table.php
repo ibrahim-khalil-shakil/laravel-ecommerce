9ee2d4c9c98c8c278369a8384459308ce6db3c29
Ibrahim Khalil Shakil <iksakil2@gmail.com>
2024-03-09 22:16:11 +0600

    added new tag column in product table

---
+++ database/migrations/2024_03_09_153546_add_tag_to_products_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->enum('tag', ['hot', 'sell', 'popular', 'trending', 'new', 'featured'])->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropColumn('tag');
+        });
+    }
+};
