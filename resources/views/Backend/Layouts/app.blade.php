c9aea84fdfe0118aad8e7dd330d39a1961ab7466
Ibrahim Khalil Shakil <iksakil2@gmail.com>
2024-03-17 14:10:53 +0600

    updated frontend app.blade

--- resources/views/Backend/Layouts/app.blade.php
+++ resources/views/Backend/Layouts/app.blade.php
@@ -28,7 +28,7 @@
                     <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
                 </li>
                 <li class="nav-item d-none d-sm-inline-block">
-                    <a href="javascript:void(0)" class="nav-link">Home</a>
+                    <a href="{{route('home')}}" class="nav-link">Home</a>
                 </li>
                 <li class="nav-item d-none d-sm-inline-block">
                     <a href="javascript:void(0)" class="nav-link">Contact</a>
